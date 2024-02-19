d30c3e7d70da90787da21baa2da638ab2d0ba531
betullkunt <betul.kunt@metu.edu.tr>
2024-02-19 15:58:24 +0300

    aaaa

--- Index.R
+++ Index.R
@@ -8,3 +8,25 @@ vec<-1:100
 vec
 class(vec)
 typeof(vec)
+
+mydice<-1:6
+mydice
+aaa<-555
+AAA<-666
+ls()
+mydice-1
+mydice*mydice
+mydice%*%mydice
+mydice%o%mydice
+dim(mydice)<-c(2,3)
+mydice
+dim(mydice)<-c(1,2,3)
+mydice
+
+m<- matrix(mydice,nrow=2,byrow=TRUE)
+m
+myarray<- array(c(1:12),dim=c(2,2,3))
+myarray
+
+now<- Sys.time()
+now
